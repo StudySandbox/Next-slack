a135b454f1b488769175647b18bf543a1d9201fd
iinsue <hinsu93@naver.com>
2024-09-14 21:16:05 +0900

    update global.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-full bg-red-200">
       <Button variant="slack">Home</Button>
-    </>
+    </div>
   );
 }
