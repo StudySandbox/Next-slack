753c606809d5db6fd76b2b4773b30c92b85053d4
iinsue <hinsu93@naver.com>
2024-09-19 04:34:08 +0900

    워크스페이스 헤더 추가

--- src/app/workspace/[workspaceId]/toolbar.tsx
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -19,7 +19,7 @@ export const Toolbar = () => {
           <span className="text-wthie text-xs">Search {data?.name}</span>
         </Button>
       </div>
-      <div className="ml-auto flex-1 items-center justify-end">
+      <div className="ml-auto flex flex-1 items-center justify-end">
         <Button variant="transparent" size="iconSm">
           <Info className="size-5 text-white" />
         </Button>
