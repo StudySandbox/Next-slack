51526b9a99c2aeb4ffbaef5a1ad34fc429f3f1cd
iinsue <hinsu93@naver.com>
2024-09-19 04:39:03 +0900

    Fix Warning: add jotai provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 
 import { Modals } from "@/components/modals";
 import { Toaster } from "@/components/ui/sonner";
+import { JotaiProvider } from "@/components/jotai-provider";
 import { ConvexClientProvider } from "@/components/convex-client-provider";
 
 import "./globals.css";
@@ -22,13 +23,15 @@ export default function RootLayout({
       <html lang="ko">
         <body>
           <ConvexClientProvider>
-            <Toaster
-              position="top-center"
-              duration={1500}
-              className="bg-white"
-            />
-            <Modals />
-            {children}
+            <JotaiProvider>
+              <Toaster
+                position="top-center"
+                duration={1500}
+                className="bg-white"
+              />
+              <Modals />
+              {children}
+            </JotaiProvider>
           </ConvexClientProvider>
         </body>
       </html>
