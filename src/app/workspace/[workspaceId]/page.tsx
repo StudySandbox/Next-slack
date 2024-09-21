8c291d2c17b634910898185a3db313945e68198d
iinsue <hinsu93@naver.com>
2024-09-21 20:50:41 +0900

    워크스페이스 페이지 채널페이지로 리다이렉트

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,9 +1,64 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
+import { useEffect, useMemo } from "react";
+import { Loader, TriangleAlert } from "lucide-react";
+
 const WorkspaceIdPage = () => {
-  return (
-    <>
-      <div>Workspace id page</div>
-    </>
-  );
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+  const [open, setOpen] = useCreateChannelModal();
+
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({
+    workspaceId,
+  });
+
+  const channelId = useMemo(() => channels?.[0]?._id, [channels]);
+
+  useEffect(() => {
+    if (workspaceLoading || channelsLoading || !workspace) return;
+
+    if (channelId) {
+      router.push(`/workspace/${workspaceId}/channel/${channelId}`);
+    } else if (!open) {
+      setOpen(true);
+    }
+  }, [
+    channelId,
+    workspaceLoading,
+    channelsLoading,
+    workspace,
+    router,
+    workspaceId,
+    open,
+    setOpen,
+  ]);
+
+  if (workspaceLoading || channelsLoading) {
+    return (
+      <div className="flex h-full flex-1 flex-col items-center justify-center gap-2">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!workspace) {
+    return (
+      <div className="flex h-full flex-1 flex-col items-center justify-center gap-2">
+        <TriangleAlert className="size-6 animate-spin text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">
+          Workspace not found
+        </span>
+      </div>
+    );
+  }
 };
 
 export default WorkspaceIdPage;
