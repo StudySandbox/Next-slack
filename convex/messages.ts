80859bfe30b759b41bad13933bd68ac677ba90f1
iinsue <hinsu93@naver.com>
2024-09-23 00:44:12 +0900

    Update Schema

--- convex/messages.ts
+++ convex/messages.ts
@@ -23,8 +23,8 @@ export const create = mutation({
     image: v.optional(v.id("_storage")),
     workspaceId: v.id("workspaces"),
     channelId: v.optional(v.id("channels")),
-    percentMessageId: v.optional(v.id("messages")),
-    // TODO: add conversationId
+    conversationId: v.optional(v.id("conversations")),
+    parentMessageId: v.optional(v.id("messages")),
   },
   handler: async (ctx, args) => {
     const userId = await getAuthUserId(ctx);
@@ -38,15 +38,27 @@ export const create = mutation({
       throw new Error("Unauthorized");
     }
 
-    // TODO: Handle conversationId
+    let _conversationId = args.conversationId;
+
+    // Only possible if we are replying in a thread in 1:1 conversation
+    if (!args.conversationId && !args.channelId && args.parentMessageId) {
+      const parentMessage = await ctx.db.get(args.parentMessageId);
+
+      if (!parentMessage) {
+        throw new Error("Parent message not found");
+      }
+
+      _conversationId = parentMessage.conversationId;
+    }
 
     const messageId = await ctx.db.insert("messages", {
       memberId: member._id,
       body: args.body,
       image: args.image,
       channelId: args.channelId,
+      conversationId: _conversationId,
       workspaceId: args.workspaceId,
-      parentMessageId: args.percentMessageId,
+      parentMessageId: args.parentMessageId,
       updatedAt: Date.now(),
     });
 
